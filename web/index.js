223fa7ca7131ede7b2695d5279cad4f7ba63904e
herbertriener <151128093+herbertriener@users.noreply.github.com>
2024-06-30 17:00:50 +0200

    Update index.js

--- web/index.js
+++ web/index.js
@@ -10,7 +10,7 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(168, 191, 214, 0.7);
     z-index: 999;
 `;
 
